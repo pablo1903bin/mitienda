71ad1b9e26cc219f16efa83468218d073759881a
Pablo rosas <pablitoypapi@gmail.com>
2023-03-04 19:45:50 -0600

    inicializa mi app tienda

---
+++ src/main/java/com/pablo/app/TestEntity.java
@@ -0,0 +1,26 @@
+package com.pablo.app;
+
+public class TestEntity {
+	
+	
+	private String Nombre;
+
+	public TestEntity() {
+
+	}
+	
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	
+	
+	
+	
+
+}
